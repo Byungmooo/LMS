a39fe0fb47ceb9dd10e0111dc1623036ec8d9591
Byungmooo <gameop12@Naver.com>
2022-09-14 15:43:35 +0900

    김병우

---
+++ LMS/src/main/java/com/gd/LMS/LmsApplication.java
@@ -0,0 +1,13 @@
+package com.gd.LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsApplication.class, args);
+	}
+
+}
---
+++ LMS/src/test/java/com/gd/LMS/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.LMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
