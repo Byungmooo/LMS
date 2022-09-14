274c78ed02e078c183e4c0f42dfdc3b9eb6d42bb
baekjihye9494 <s2jhye@naver.com>
2022-09-14 18:35:54 +0900

    [백지혜] application

--- LMS/src/main/java/com/gd/LMS/LmsApplication.java
+++ LMS/src/main/java/com/gd/LMS/LmsApplication.java
@@ -3,6 +3,7 @@ package com.gd.LMS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LmsApplication {
 
