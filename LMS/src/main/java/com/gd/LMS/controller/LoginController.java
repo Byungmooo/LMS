4c154c6f86e97a33fd0ff00bc5203d31159f98af
Byungmooo <gameop12@Naver.com>
2022-09-16 10:04:17 +0900

    [김병우] Login Form에서 회원가입, index연결

---
+++ LMS/src/main/java/com/gd/LMS/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.gd.LMS.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/Login")
+	public String Login() {
+		
+		return "Login";
+	}
+
+}
